510791faa17a362faeab72bd367ca26e5b0ad5f6
Ayooluwa Isaiah <ayisaiah@gmail.com>
2022-09-02 10:28:23 +0100

    update demo

--- main.go
+++ main.go
@@ -9,7 +9,9 @@ import (
 	"log"
 	"math"
 	"net/http"
+	"net/http/httputil"
 	"net/url"
+	"os"
 	"strconv"
 	"time"
 )
@@ -65,7 +67,23 @@ func (s *Search) PreviousPage() int {
 	return s.CurrentPage() - 1
 }
 
+type handlerWithError func(w http.ResponseWriter, r *http.Request) error
+
+func (fn handlerWithError) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	err := fn(w, r)
+	if err != nil {
+		log.Println(err)
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+}
+
 func indexHandler(w http.ResponseWriter, r *http.Request) error {
+	if r.URL.Path != "/" {
+		http.NotFound(w, r)
+		return nil
+	}
+
 	buf := &bytes.Buffer{}
 	err := tpl.Execute(buf, nil)
 	if err != nil {
@@ -96,7 +114,12 @@ func searchWikipedia(
 	defer resp.Body.Close()
 
 	if resp.StatusCode != http.StatusOK {
-		return nil, nil
+		respData, _ := httputil.DumpResponse(resp, true)
+
+		return nil, fmt.Errorf(
+			"non 200 OK response from Wikipedia API: %s",
+			string(respData),
+		)
 	}
 
 	body, err := io.ReadAll(resp.Body)
@@ -157,42 +180,42 @@ func searchHandler(w http.ResponseWriter, r *http.Request) error {
 	}
 
 	_, err = buf.WriteTo(w)
-
-	return err
-}
-
-type handlerWithError func(w http.ResponseWriter, r *http.Request) error
-
-func (fn handlerWithError) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	err := fn(w, r)
 	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
+		return err
 	}
+
+	return nil
 }
 
 func htmlSafe(str string) template.HTML {
 	return template.HTML(str)
 }
 
-func init() {
-	var err error
+var err error
 
+func init() {
 	tpl, err = template.New("index.html").Funcs(template.FuncMap{
 		"htmlSafe": htmlSafe,
 	}).ParseFiles("index.html")
 	if err != nil {
-		log.Fatal(err)
+		log.Fatal("Unable to initialize HTML templates")
 	}
 }
 
 func main() {
 	fs := http.FileServer(http.Dir("assets"))
 
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "3000"
+	}
+
 	mux := http.NewServeMux()
 	mux.Handle("/assets/", http.StripPrefix("/assets/", fs))
 	mux.Handle("/search", handlerWithError(searchHandler))
 	mux.Handle("/", handlerWithError(indexHandler))
 
-	log.Fatal(http.ListenAndServe(":3000", mux))
+	log.Printf("Starting Wikipedia App Server on port '%s'", port)
+
+	log.Fatal(http.ListenAndServe(":"+port, mux))
 }
