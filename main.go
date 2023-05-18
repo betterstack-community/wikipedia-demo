cd1b3aa7c978547b8138eeb1e8c9129b9ec768df
Ayooluwa Isaiah <ayisaiah@gmail.com>
2023-05-18 12:25:34 +0100

    Fix search query issue

--- main.go
+++ main.go
@@ -123,7 +123,7 @@ func searchWikipedia(
 	endpoint := fmt.Sprintf(
 		"https://en.wikipedia.org/w/api.php?action=query&list=search&prop=info&inprop=url&utf8=&format=json&origin=*&srlimit=%d&srsearch=%s&sroffset=%d",
 		pageSize,
-		searchQuery,
+		url.QueryEscape(searchQuery),
 		resultsOffset,
 	)
 
